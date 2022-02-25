3ebf21a0fb850f5a5b821b9a110548cfbdbf6ba2
Aza Maulana <azamaulanaaa@gmail.com>
2022-02-25 13:47:01 +0700

    build(nix): add nix shell
    add minimum package to develop golang program

---
+++ shell.nix
@@ -0,0 +1,11 @@
+{pkgs ? import <nixpkgs> {}} :
+pkgs.mkShell {
+    packages = with pkgs; [
+        go_1_17
+        gopls
+    ];
+    shellHook = ''
+        export PATH=$PATH:$(go env GOPATH)/bin
+        export GOFLAGS=-tags=unit,integration
+    '';
+}
